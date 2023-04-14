c5015817c24a0c26f9aad738b7e3a5058af7b2a0
Pavel Mezencev <mezencev24@mail.ru>
2023-04-14 15:20:59 +0300

    add create, delete and clear feature

--- app/main.js
+++ app/main.js
@@ -1,32 +1,61 @@
 import { database } from './firebase.config';
-import { ref, push, onValue } from 'firebase/database';
+import { ref, push, onValue, remove } from 'firebase/database';
 import '../style/index.scss';
 
 const shoppingListInDb = ref(database, 'shopping_list');
 const form = document.querySelector('#form');
 const mainInput = document.querySelector('#main-input');
 const shoppingList = document.querySelector('.shopping-list');
+const trashBtn = document.querySelector('.btn-trash');
+const cartMsg = document.querySelector('.cart-msg');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const { value } = mainInput;
-  push(shoppingListInDb, value);
-  mainInput.value = '';
+form.addEventListener('submit', addToCart);
+shoppingList.addEventListener('click', removeItemFromsCart);
+trashBtn.addEventListener('click', clearShoppingCart);
+
+onValue(shoppingListInDb, updateShopList, {
+  onlyOnce: false,
 });
 
-onValue(
-  shoppingListInDb,
-  (snapshot) => {
-    shoppingList.innerHTML = '';
-    snapshot.forEach((childSnapshot) => {
-      const childKey = childSnapshot.key;
-      const childData = childSnapshot.val();
-      const shoppingItem = `<li data-id=${childKey}>${childData}</li>`;
-      shoppingList.insertAdjacentHTML('beforeend', shoppingItem);
-      console.log(childData);
-    });
-  },
-  {
-    onlyOnce: false,
+// FUNCTIONS
+function addToCart(e) {
+  e.preventDefault();
+  const { value } = mainInput;
+  if (value.trim() === '') return;
+  push(shoppingListInDb, value);
+  clearInput(mainInput);
+}
+
+function updateShopList(snapshot) {
+  shoppingList.innerHTML = '';
+  if (snapshot.size > 0) {
+    cartMsg.classList.add('hide');
+  } else {
+    cartMsg.classList.remove('hide');
   }
-);
+  snapshot.forEach((childSnapshot) => {
+    const childKey = childSnapshot.key;
+    const childData = childSnapshot.val();
+    renderNewItemToCart(shoppingList, { childKey, childData });
+  });
+}
+
+function renderNewItemToCart(listNode, { childKey, childData }) {
+  const shoppingItem = `<li class='cart-item' data-id=${childKey}>${childData}</li>`;
+  listNode.insertAdjacentHTML('afterbegin', shoppingItem);
+}
+
+function clearInput(input) {
+  input.value = '';
+}
+
+async function removeItemFromsCart(e) {
+  if (!e.target.classList.contains('cart-item')) return;
+  const item = e.target;
+  const itemId = item.dataset.id;
+  await remove(ref(database, `/shopping_list/${itemId}`));
+}
+
+async function clearShoppingCart() {
+  await remove(ref(database, '/shopping_list'));
+}
